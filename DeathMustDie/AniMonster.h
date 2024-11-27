0bac8fd13f260af57eaf877663d7a0e852900745
박하영 <rudal4277@naver.com>
2024-11-28 03:14:28 +0900

    AniMonster 추가

---
+++ DeathMustDie/AniMonster.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "GameObject.h"
+
+class AniMonster : public GameObject
+{
+protected:
+	sf::Sprite body;
+	Animator animator;
+	AnimationClip idle;
+public:
+	AniMonster(const std::string& name = "");
+	~AniMonster() = default;
+
+	void SetPosition(const sf::Vector2f& pos) override;
+	void SetRotation(float angle) override;
+	void SetScale(const sf::Vector2f& scale) override;
+
+	void SetOrigin(Origins preset) override;
+	void SetOrigin(const sf::Vector2f& newOrigin) override;
+
+	void Init() override;
+	void Release() override;
+	void Reset() override;
+	void Update(float dt) override;
+	void Draw(sf::RenderWindow& window) override;
+};
+
